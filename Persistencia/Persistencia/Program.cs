16628e0dcf0fe92189625997d14741a8e9c24aac
leonardo.002gamer-crypto <jdrojas1608@gmail.com>
2026-04-17 14:52:52 -0400

    agregamosel directorio

--- Persistencia/Persistencia/Program.cs
+++ Persistencia/Persistencia/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: usuario
+ * Usuario: Leonardo Rivas
  * Fecha: 17/4/2026
  * Hora: 2:19 p. m.
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace Persistencia
 {
@@ -16,8 +17,21 @@ namespace Persistencia
 		{
 			Console.WriteLine("Taller");
 			
-			// TODO: Implement Functionality Here
+			// directorio
 			
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"DatosIujo");
+			
+			
+			string rutaReportes = Path.Combine(rutaRaiz, "Reportes");
+			
+			Console.Write(rutaRaiz);
+			Console.Write(rutaReportes);
+			
+			if(!Directory.Exists(rutaReportes)){
+				//Crea el directorio reportes
+				Directory.CreateDirectory(rutaReportes);
+				Console.WriteLine ("Directorio Creado correctamente");
+			   }
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
